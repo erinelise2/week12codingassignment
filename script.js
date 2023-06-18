4a0263994d922b02c26c8a1abb288a105a69d157
Erin Brickman <eelise187@Erins-MBP.attlocal.net>
2023-06-18 08:10:04 -0700

    Attempting to create a to do list CRUD app based on information from the lab. db.json not linking

---
+++ script.js
@@ -0,0 +1,54 @@
+// Declaring a URL endpoint. Can be an API (using AJAX) or local db.json. If you use local, *** type: json-server --watch db.json into terminal. Then use that url below.  NOT SURE HOW TO GET THIS TO WORK //
+// const taskdatabase_URL = 'http://localhost:3000/'
+
+// Create a code that loops over data and adds information to DOM //
+$.get(taskdatabase_URL).then((data) =>
+  data.map((targetTask) => {
+    $('tbody').append(
+      $(`
+    <tr>
+      <td>${task.id}</td>
+      <td>${task.toDoTask}</td>
+      <td>${task.toDoNote}</td>
+      <td>${task.toDoDeadline}</td>
+      <td>
+        <button onclick="deleteTask(${task.id})"}>🗑</button>
+      </td>
+    </tr>`)
+    )
+  })
+)
+
+//Post/Adding new tasks //
+$('#task').click(function () {
+    $.post(taskdatabase_URL, {
+      toDoTask: $('#newTask').val(),
+      toDoNote: $('#newNote').val(),
+      toDoDeadline: $('#newDeadline').val(),
+    })
+  })
+
+//Deleting existing task ASCII trash bin: 🗑 from lab //
+
+function deleteTask(id) {
+    $.ajax(`${taskDatabase_URL}/${id}`, {
+      type: 'DELETE',
+    })
+  }
+
+//Updating information //
+function updateTask() {
+    id = $('#updateId').val()
+  
+    $.ajax(`${taskdatabase_URL}/${id}`, {
+      method: 'PUT',
+      data: {
+        toDoTask: $('#updateTask').val(),
+        toDoNote: $('#updateNote').val(),
+        toDoDealine: $('#updateDeadline').val(),
+      },
+    })
+  }
+  
+  // event listener to update task when button is clicked //
+  $('#updateTask').click(updateTask)
