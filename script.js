cdbbf6cf1e5b950393b86e05178ebc723e222ce2
Erin Brickman <eelise187@Erins-MBP.attlocal.net>
2023-06-18 12:11:04 -0700

    table notes for possible adds/changes when it is connected

--- script.js
+++ script.js
@@ -10,8 +10,9 @@ $.get(taskdatabase_URL).then((data) =>
       <td>${task.id}</td>
       <td>${task.toDoTask}</td>
       <td>${task.toDoNote}</td>
-      <td>${task.toDoDeadline}</td>
+      <td>${task.toDoDeadline}</td> //a way to rank by closest date at the top? //
       <td>${task.toDoStatus}</td>
+    //   could also make this a button... need to create the function to mark off? could this include a date //
       <td>
         <button onclick="deleteTask(${task.id})"}>🗑</button>
       </td>
