3fc75e500ab3291b48e0beb0ded8f1f576a26ea8
Erin Brickman <eelise187@Erins-MBP.attlocal.net>
2023-06-18 21:05:55 -0700

    more formatting and selected a sound to try to add when the database is connected and I can target the complete radio button

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ $.get(taskdatabase_URL).then((data) =>
       <td>${task.toDoStatus}</td>
       <td>${task.toDoNote}</td>
       <td>${task.toDoDeadline}</td> //a way to rank by closest date at the top? //
-      <td>${task.toDoComplete}</td>
+      <td>${task.toDoCompleteDate}</td>
     //   could also make this a button... need to create the function to mark off? could this include a date //
       <td>
         <button onclick="deleteTask(${task.id})"}>🗑</button>
@@ -26,9 +26,10 @@ $.get(taskdatabase_URL).then((data) =>
 $('#task').click(function () {
     $.post(taskdatabase_URL, {
       toDoTask: $('#newTask').val(),
-      toDoTask: $('#newStatus').val,
+      toDoStatus: $('#newStatus').val,
       toDoNote: $('#newNote').val(),
       toDoDeadline: $('#newDeadline').val(),
+      toDoCompleteDate: $('newCompleteDate').val,
     })
   });
 
@@ -51,10 +52,13 @@ function updateTask() {
         toDoStatus: $('#updateStatus').val,
         toDoNote: $('#updateNote').val(),
         toDoDeadline: $('#updateDeadline').val(),
-        toDoStatus: $('#updateComplete').val(),
+        toDoCompleteDate: $('#updateCompleteDate').val(),
       },
     })
   };
   
   // event listener to update task when button is clicked //
   $('#updateTask').click(updateTask)
+
+  // something fun I found on a youtube https://www.youtube.com/watch?v=fPew9OI2PnA and used for tic tac toe game. Found a sound that works for my page https://mixkit.co/free-sound-effects/applause/ and want to play it when a task is marked as complete... /
+  const taskCompleteSound = new Audio("sounds/mixkit-small-crowd-ovation-437.wav");
